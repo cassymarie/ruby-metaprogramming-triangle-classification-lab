d1c62eb22f6531582e8cfac8fcbc0b4e339fd8b4
cassymarie <cassy2205@gmail.com>
2020-11-06 10:32:00 -0700

    commit tree 2804670eb37f7f1a8ffceda2ee0de73f43ad3dc5

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,48 @@
+
+
+
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c, :sides, :kind
+
+
+    def initialize(a,b,c)
+
+      raise TriangleError unless valid?([a,b,c]) 
+      raise TriangleError unless !inequality?([a,b,c])
+
+      @a = a
+      @b = b
+      @c = c
+      @sides = [@a, @b, @c]
+      type
+    end
+
+    def type
+      total_uniq = @sides.uniq.size
+      case total_uniq
+      when 1
+        @kind = :equilateral
+      when 2
+        @kind = :isosceles
+      when 3
+        @kind = :scalene
+      else
+        @kind = :invalid
+      end
+      @kind
+    end
+
+    def valid?(num_arr)
+      num_arr.all? {|x| x > 0} && !num_arr.empty? && num_arr.size == 3 ? true : false
+    end
+
+    def inequality?(arr)
+      (arr[0] + arr[1] <= arr[2] || arr[0] + arr[2] <= arr[1] || arr[1] + arr[2] <= arr[0]) ? true : false
+    end
+
+    class TriangleError < StandardError
+      ## dont really know after this -- kinda confused.
+      message = "Hey - thats wrong!"
+    end
+  
 end
